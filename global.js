e90b7a39a95a40e7a3364cb7d994d7e2a9d0dee9
KanishkHari <kanishkhari0@gmail.com>
2025-10-15 19:13:38 -0700

    update global js and css file

--- global.js
+++ global.js
@@ -1,6 +1,6 @@
 console.log("IT'S ALIVE!");
 
-function $$(selector, context = document) {
+function $$(selector, context = document) { 
   return Array.from(context.querySelectorAll(selector));
 }
 
