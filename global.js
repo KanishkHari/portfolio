1a1d95ddc9be2c26661c250b78302bea88864ebb
KanishkHari <kanishkhari0@gmail.com>
2025-10-24 10:47:34 -0700

    Update global.js

--- global.js
+++ global.js
@@ -137,7 +137,7 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
   });
 }
 
-export async function fetchGitHubData(username) {
+export async function fetchGithubData(username) {
   return fetchJSON(`https://api.github.com/users/${username}`);
   // return statement here
 }
