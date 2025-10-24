702cb3ddb60b3ea18b643b056eda0632072f1a98
KanishkHari <kanishkhari0@gmail.com>
2025-10-24 00:27:22 -0700

    Update global.js

--- global.js
+++ global.js
@@ -137,4 +137,10 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
   });
 }
 
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+  // return statement here
+}
+
+
 
