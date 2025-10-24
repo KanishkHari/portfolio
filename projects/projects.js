fa2955fdcc560db507a3f208d711fa69db243f62
KanishkHari <kanishkhari0@gmail.com>
2025-10-24 10:33:13 -0700

    revert changes

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,14 @@
 import { fetchJSON, renderProjects } from '../global.js';
-const projects = await fetchJSON('../lib/projects.json');
-const projectsContainer = document.querySelector('.projects');
-renderProjects(projects, projectsContainer, 'h2');
+async function loadProjects() {
+  const projects = await fetchJSON('../lib/projects.json');
+  console.log('Projects:', projects);
+  const projectsContainer = document.querySelector('.projects');
+  renderProjects(projects, projectsContainer, 'h2');
+
+  const titleElement = document.querySelector('.projects-title');
+  if (titleElement) {
+    titleElement.textContent = `Projects (${projects.length})`;
+  }
+}
+
+loadProjects();
