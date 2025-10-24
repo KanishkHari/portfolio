a1e4ec0daf0586daea82b14b83a6bc09f847dd1c
KanishkHari <kanishkhari0@gmail.com>
2025-10-24 00:20:04 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
+async function loadLatest() {
+  const projects = await fetchJSON('./lib/projects.json');
+  const latestProjects = projects.slice(0, 3);
+  const projectsContainer = document.querySelector('.projects');
+  renderProjects(latestProjects, projectsContainer, 'h2');
+}
+
+loadLatestProjects();
+
