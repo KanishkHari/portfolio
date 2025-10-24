f1718690c9ccf5f2c8adedc5a538b3c10a068380
KanishkHari <kanishkhari0@gmail.com>
2025-10-24 00:30:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ async function loadLatest() {
   const latestProjects = projects.slice(0, 3);
   const projectsContainer = document.querySelector('.projects');
   renderProjects(latestProjects, projectsContainer, 'h2');
+
+  // step 3 
+  const githubData = await fetchGithubData('kanishkhari');
+  const profileStats = document.querySelector('#profile-stats');
+
+  if (profileStats) {
+  profileStats.innerHTML = `
+        <dl>
+          <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+          <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+          <dt>Followers:</dt><dd>${githubData.followers}</dd>
+          <dt>Following:</dt><dd>${githubData.following}</dd>
+          <dt>Starred Repos:</dt><dd>${githubData.starred_url}</dd>
+        </dl>
+    `;
+}
+
 }
 
 loadLatestProjects();
