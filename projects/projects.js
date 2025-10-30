0a6bf1369cca98b7aa46c756c09f8f47a260e106
KanishkHari <kanishkhari0@gmail.com>
2025-10-30 01:10:38 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -13,27 +13,58 @@ async function loadProjects() {
 
 loadProjects();
 
+// import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+// let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+// let arc = arcGenerator({
+//   startAngle: 0,
+//   endAngle: 2 * Math.PI,
+// });
+// d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
+
+// let data = [1, 2];
+// let total = 0;
+
+// for (let d of data) {
+//   total += d;
+// }
+// let angle = 0;
+// let arcData = [];
+
+// for (let d of data) {
+//   let endAngle = angle + (d / total) * 2 * Math.PI;
+//   arcData.push({ startAngle: angle, endAngle });
+//   angle = endAngle;
+// }
+// let arcs = arcData.map((d) => arcGenerator(d));
+
+// arcs.forEach((arc) => {
+//   // TODO, fill in step for appending path to svg using D3
+
+// });
+
+// Step 1.3 - import D3
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
-});
-d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
-
+// Step 1.4 - basic data
 let data = [1, 2];
-let total = 0;
 
-for (let d of data) {
-  total += d;
-}
-let angle = 0;
-let arcData = [];
+// Step 1.3 - create an arc generator
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
+// Step 1.4 - use D3 pie helper
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
+
+// Step 1.5 - color scale
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+// draw arcs
+arcs.forEach((arc, idx) => {
+  d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors(idx));
+});
+
