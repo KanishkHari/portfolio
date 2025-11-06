b3d6997904267f6840e219d7998e9f0c8ad7c295
KanishkHari <kanishkhari0@gmail.com>
2025-11-05 23:50:18 -0800

    majority of step 2

--- meta/main.js
+++ meta/main.js
@@ -54,11 +54,11 @@ function renderCommitInfo(data, commits) {
   dl.append('dt').text('Total Commits');
   dl.append('dd').text(commits.length);
 // num files
-  const files = d3.group(data, d => d.file);
+  const files = d3.group(data, (d) => d.file);
   dl.append('dt').text('Number of Files');
   dl.append('dd').text(files.size);
   // avg lines per commit
-  const avgLines = d3.mean(commits, d => d.totalLines);
+  const avgLines = d3.mean(commits, (d) => d.totalLines);
   dl.append('dt').text('Avg Lines per Commit');
   dl.append('dd').text(Math.round(avgLines));
   // work by time period
@@ -80,8 +80,68 @@ function renderCommitInfo(data, commits) {
 
 }
 
+function renderScatterPlot(data, commits) {
+  // Put all the JS code of Steps inside this function
+  const width = 1000;
+  const height = 600;
+}
+
+const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+// create xScale and yScale
+const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([0, width])
+    .nice();
+const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+const dots = svg.append('g').attr('class', 'dots');
+dots
+    .selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue')
+
+const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+//usableArea
+const usableArea = {
+  top: margin.top,
+  right: width - margin.right,
+  bottom: height - margin.bottom,
+  left: margin.left,
+  width: width - margin.left - margin.right,
+  height: height - margin.top - margin.bottom,
+};
+
+xScale.range([usableArea.left, usableArea.right]);
+yScale.range([usableArea.bottom, usableArea.top]);
+
+// create axes
+const xAxis = d3.axisBottom(xScale)
+const yAxis = d3
+  .axisLeft(yScale)
+  .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+// add X axis
+svg
+  .append('g')
+  .attr('transform', `translate(0, ${usableArea.bottom})`)
+  .call(xAxis);
+// add y axis
+svg
+  .append('g')
+  .attr('transform', `translate(${usableArea.left}, 0)`)
+  .call(yAxis);
 
 let data = await loadData();
 let commits = processCommits(data);
 
 renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);
