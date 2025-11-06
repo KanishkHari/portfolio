1d86a04e65d12d12ce27694d5584a7128e2f004b
KanishkHari <kanishkhari0@gmail.com>
2025-11-06 00:06:56 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -91,64 +91,64 @@ function renderScatterPlot(data, commits) {
     .attr('viewBox', `0 0 ${width} ${height}`)
     .style('overflow', 'visible');
 // create xScale and yScale
-const xScale = d3
+  const xScale = d3
     .scaleTime()
     .domain(d3.extent(commits, (d) => d.datetime))
     .range([0, width])
     .nice();
-const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
-
-const dots = svg.append('g').attr('class', 'dots');
-
-dots
+    
+  const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+  
+  const dots = svg.append('g').attr('class', 'dots');
+  dots
     .selectAll('circle')
     .data(commits)
     .join('circle')
     .attr('cx', (d) => xScale(d.datetime))
     .attr('cy', (d) => yScale(d.hourFrac))
     .attr('r', 5)
-    .attr('fill', 'steelblue')
-
-const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+    .attr('fill', 'steelblue');
+    
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
 //usableArea
-const usableArea = {
-  top: margin.top,
-  right: width - margin.right,
-  bottom: height - margin.bottom,
-  left: margin.left,
-  width: width - margin.left - margin.right,
-  height: height - margin.top - margin.bottom,
-};
-
-xScale.range([usableArea.left, usableArea.right]);
-yScale.range([usableArea.bottom, usableArea.top]);
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+  
+  xScale.range([usableArea.left, usableArea.right]);
+  yScale.range([usableArea.bottom, usableArea.top]);
 
 
 // add gridlines before axes
 
-const gridLines = svg
-  append('g')
-  .attr('class', 'gridlines')
-  .attr('transform', `translate(${usableArea.left}, 0)`);
+  const gridLines = svg
+    append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usableArea.left}, 0)`);
 
-gridLines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+  gridLines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
 
 // create axes
-const xAxis = d3.axisBottom(xScale)
-const yAxis = d3
-  .axisLeft(yScale)
-  .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+  const xAxis = d3.axisBottom(xScale)
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
 
 // add X axis
-svg
-  .append('g')
-  .attr('transform', `translate(0, ${usableArea.bottom})`)
-  .call(xAxis);
-// add y axis
-svg
-  .append('g')
-  .attr('transform', `translate(${usableArea.left}, 0)`)
-  .call(yAxis);
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+  // add y axis
+  svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
 }
 
 
