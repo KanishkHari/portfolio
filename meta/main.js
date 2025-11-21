474797e7e2862ecb0689df25fd1c01fcb16b84bc
KanishkHari <kanishkhari0@gmail.com>
2025-11-21 08:58:18 -0800

    update main.js and log new commits

--- meta/main.js
+++ meta/main.js
@@ -381,9 +381,12 @@ function onTimeSliderChange() {
 function updateFileDisplay(filteredCommits) {
   let lines = filteredCommits.flatMap((d) => d.lines);
 
-  let files = d3.groups(lines, (d) => d.file).map(([name, lines]) => {
+  let files = d3
+  .groups(lines, (d) => d.file)
+  .map(([name, lines])=> {
     return { name, lines };
-  });
+  })
+  .sort((a, b) => b.lines.length - a.lines.length);
 
   let filesContainer = d3
     .select("#files")
